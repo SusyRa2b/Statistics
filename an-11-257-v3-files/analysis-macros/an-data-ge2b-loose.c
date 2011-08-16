5d52149020fcf47a92c819cb3595dba8c28aa95b
owen <>
2011-08-16 03:06:19 +0000

    macros for data fits.

---
+++ an-11-257-v3-files/analysis-macros/an-data-ge2b-loose.c
@@ -0,0 +1,109 @@
+
+
+   void an_data_ge2b_loose() {
+
+      gROOT->LoadMacro("ra2bRoostatsClass4.c+") ;
+
+      float low,high ;
+      float susylow, susyhigh ;
+
+      // ra2bRoostatsClass4( bool ArgUseSigTtwjVar=false, bool ArgUseLdpVars=true ) ;
+
+      ra2bRoostatsClass4 rfitNoSusy(1,0,2) ;
+      rfitNoSusy.initialize("an-11-257-v3-files/input-files/byhand-data-ge2b-loose.txt") ;
+      rfitNoSusy.setAndFixSusySig(0.) ;
+
+      rfitNoSusy.doFit() ;
+      printf("\n\n ======== best fit, no susy included ==============\n\n") ;
+      rfitNoSusy.parameterSnapshot() ;
+      rfitNoSusy.fitQualityPlot(0,"an-11-257-v3-files/output-files/fitQual-unblind-BestFit-NoSusy-ge2b-loose-sel.png") ;
+      rfitNoSusy.fitQualityPlot(1,"an-11-257-v3-files/output-files/fitQualNorm-unblind-BestFit-NoSusy-ge2b-loose-sel.png") ;
+      rfitNoSusy.reinitialize() ;
+      rfitNoSusy.setAndFixSusySig(0.) ;
+      rfitNoSusy.doFit() ;
+      rfitNoSusy.profilettwjSig(low,high,1,"an-11-257-v3-files/output-files/ttwjSigProf-unblind-NoSusy-ge2b-loose-sel.png", 50. ) ;
+      rfitNoSusy.reinitialize() ;
+      rfitNoSusy.setAndFixSusySig(0.) ;
+      rfitNoSusy.doFit() ;
+      rfitNoSusy.profileqcdSig(low,high,1,"an-11-257-v3-files/output-files/qcdSigProf-unblind-NoSusy-ge2b-loose-sel.png", 5. ) ;
+      rfitNoSusy.reinitialize() ;
+      rfitNoSusy.setAndFixSusySig(0.) ;
+      rfitNoSusy.doFit() ;
+      rfitNoSusy.profileZnnSig(low,high,1,"an-11-257-v3-files/output-files/znnSigProf-unblind-NoSusy-ge2b-loose-sel.png", 20. ) ;
+
+
+
+   //-------------------------
+
+      ra2bRoostatsClass4 rfitSusyFloat(1,0,2) ;
+      rfitSusyFloat.initialize("an-11-257-v3-files/input-files/byhand-data-ge2b-loose.txt") ;
+      rfitSusyFloat.setSusyScanPoint("an-11-257-v3-files/input-files/signalSyst.LM9.ge2bLoose.1143invpb.dat",0,0) ;
+
+      rfitSusyFloat.doFit() ;
+      printf("\n\n ======== best fit, susy floating ==============\n\n") ;
+      rfitSusyFloat.parameterSnapshot() ;
+      rfitSusyFloat.fitQualityPlot(0,"an-11-257-v3-files/output-files/fitQual-unblind-BestFit-susyFloat-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFloat.fitQualityPlot(1,"an-11-257-v3-files/output-files/fitQualNorm-unblind-BestFit-susyFloat-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFloat.profileSusySig(susylow,susyhigh,1,"an-11-257-v3-files/output-files/susySigProf-unblind-susyFloat-lm9-ge2b-loose-sel.png", 50. ) ;
+      rfitSusyFloat.reinitialize() ;
+      rfitSusyFloat.doFit() ;
+      rfitSusyFloat.profilettwjSig(low,high,1,"an-11-257-v3-files/output-files/ttwjSigProf-unblind-susyFloat-lm9-ge2b-loose-sel.png", 50. ) ;
+      rfitSusyFloat.reinitialize() ;
+      rfitSusyFloat.doFit() ;
+      rfitSusyFloat.profileqcdSig(low,high,1,"an-11-257-v3-files/output-files/qcdSigProf-unblind-susyFloat-lm9-ge2b-loose-sel.png", 5. ) ;
+      rfitSusyFloat.reinitialize() ;
+      rfitSusyFloat.doFit() ;
+      rfitSusyFloat.profileZnnSig(low,high,1,"an-11-257-v3-files/output-files/znnSigProf-unblind-susyFloat-lm9-ge2b-loose-sel.png", 20. ) ;
+
+
+      rfitSusyFloat.reinitialize() ;
+      rfitSusyFloat.doFit() ;
+      rfitSusyFloat.setAndFixSusySig(susylow) ;
+      rfitSusyFloat.doFit() ;
+      printf("\n\n ======== lower limit , susy floating==============\n\n") ;
+      rfitSusyFloat.parameterSnapshot() ;
+      rfitSusyFloat.fitQualityPlot(0,"an-11-257-v3-files/output-files/fitQual-unblind-susyFloat-LowerLimit-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFloat.fitQualityPlot(1,"an-11-257-v3-files/output-files/fitQualNorm-unblind-susyFloat-LowerLimit-lm9-ge2b-loose-sel.png") ;
+
+      rfitSusyFloat.setAndFixSusySig(susyhigh) ;
+      rfitSusyFloat.doFit() ;
+      printf("\n\n ======== upper limit , susy floating==============\n\n") ;
+      rfitSusyFloat.parameterSnapshot() ;
+      rfitSusyFloat.fitQualityPlot(0,"an-11-257-v3-files/output-files/fitQual-unblind-susyFloat-UpperLimit-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFloat.fitQualityPlot(1,"an-11-257-v3-files/output-files/fitQualNorm-unblind-susyFloat-UpperLimit-lm9-ge2b-loose-sel.png") ;
+
+
+   //-------------------------
+
+      ra2bRoostatsClass4 rfitSusyFix(1,0,2) ;
+      rfitSusyFix.initialize("an-11-257-v3-files/input-files/byhand-data-ge2b-loose.txt") ;
+      rfitSusyFix.setSusyScanPoint("an-11-257-v3-files/input-files/signalSyst.LM9.ge2bLoose.1143invpb.dat",0,0) ;
+      rfitSusyFix.setAndFixSusySigToPredictedValue() ;
+
+      rfitSusyFix.doFit() ;
+      printf("\n\n ======== best fit, susy fixed ==============\n\n") ;
+      rfitSusyFix.parameterSnapshot() ;
+      rfitSusyFix.fitQualityPlot(0,"an-11-257-v3-files/output-files/fitQual-unblind-BestFit-susyFixed-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFix.fitQualityPlot(1,"an-11-257-v3-files/output-files/fitQualNorm-unblind-BestFit-susyFixed-lm9-ge2b-loose-sel.png") ;
+      rfitSusyFix.reinitialize() ;
+      rfitSusyFix.setAndFixSusySigToPredictedValue() ;
+      rfitSusyFix.doFit() ;
+      rfitSusyFix.profilettwjSig(low,high,1,"an-11-257-v3-files/output-files/ttwjSigProf-unblind-susyFixed-lm9-ge2b-loose-sel.png", 50. ) ;
+      rfitSusyFix.reinitialize() ;
+      rfitSusyFix.setAndFixSusySigToPredictedValue() ;
+      rfitSusyFix.doFit() ;
+      rfitSusyFix.profileqcdSig(low,high,1,"an-11-257-v3-files/output-files/qcdSigProf-unblind-susyFixed-lm9-ge2b-loose-sel.png", 5. ) ;
+      rfitSusyFix.reinitialize() ;
+      rfitSusyFix.setAndFixSusySigToPredictedValue() ;
+      rfitSusyFix.doFit() ;
+      rfitSusyFix.profileZnnSig(low,high,1,"an-11-257-v3-files/output-files/znnSigProf-unblind-susyFixed-lm9-ge2b-loose-sel.png", 20. ) ;
+
+
+
+
+
+
+
+
+
+   }
