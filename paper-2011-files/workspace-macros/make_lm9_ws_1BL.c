289b191a92d08f9ddaec7a8aec53e43e8db77930
owen <>
2011-11-29 16:17:26 +0000

    Owen : adding some workspace tools for the paper version of the analysis

---
+++ paper-2011-files/workspace-macros/make_lm9_ws_1BL.c
@@ -0,0 +1,18 @@
+
+void make_lm9_ws_1BL() {
+
+  gROOT->LoadMacro("ra2bRoostatsClass7.c+") ;
+
+  ra2bRoostatsClass7 ra2b(1,0) ;
+
+  ra2b.initialize("paper-2011-files/input-files/byhand-data-1BL.txt",
+                  "paper-2011-files/input-files/signalSyst.LM9.ge1bLoose.dat", 0., 0., false, 1.0 ) ;
+
+  gSystem->Exec("mkdir -p output-files") ;
+  gSystem->Exec("mv output-files/ws-lm9-1BL.root output-files/ws-lm9-1BL.root-old") ;
+  gSystem->Exec("mv ws.root output-files/ws-lm9-1BL.root") ;
+
+}
+
+
+
---
+++ paper-2011-files/workspace-macros/make_lm9_ws_1BT.c
@@ -0,0 +1,18 @@
+
+void make_lm9_ws_1BT() {
+
+  gROOT->LoadMacro("ra2bRoostatsClass7.c+") ;
+
+  ra2bRoostatsClass7 ra2b(1,0) ;
+
+  ra2b.initialize("paper-2011-files/input-files/byhand-data-1BT.txt",
+                  "paper-2011-files/input-files/signalSyst.LM9.ge1bTight.dat", 0., 0., false, 1.0 ) ;
+
+  gSystem->Exec("mkdir -p output-files") ;
+  gSystem->Exec("mv output-files/ws-lm9-1BT.root output-files/ws-lm9-1BT.root-old") ;
+  gSystem->Exec("mv ws.root output-files/ws-lm9-1BT.root") ;
+
+}
+
+
+
