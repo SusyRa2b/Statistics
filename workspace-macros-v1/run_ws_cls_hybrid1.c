cfe71f3c7c772567b76c517a54f54e71c20b6a8d
gaz <>
2011-12-16 17:56:19 +0000

    added ws_cls_hybrid1_ag.c

--- workspace-macros-v1/run_ws_cls_hybrid1.c
+++ workspace-macros-v1/run_ws_cls_hybrid1.c
@@ -5,7 +5,8 @@
 
       gROOT->LoadMacro("ws_cls_hybrid1.c+") ;
 
-      TFile* outf = new TFile("cls-expected-2BT.root","recreate") ;
+      //TFile* outf = new TFile("cls-expected-2BT.root","recreate") ;
+      TFile* outf = new TFile("cls-observed-2BT.root","recreate") ;
 
       gDirectory->pwd() ;
 
@@ -20,15 +21,17 @@
 
     //-------
 
-      for ( int poii=0; poii<10; poii++ ) {
+      for ( int poii=0; poii<14; poii++ ) {
 
          poiVal = 5. + 5*poii ;
 
          isBgonlyStudy = false ;
-         ws_cls_hybrid1( "output-files/expected-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
+         //ws_cls_hybrid1( "output-files/expected-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
+         ws_cls_hybrid1( "output-files/observed-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
 
          isBgonlyStudy = true ;
-         ws_cls_hybrid1( "output-files/expected-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
+         //ws_cls_hybrid1( "output-files/expected-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
+         ws_cls_hybrid1( "output-files/observed-ws-lm9-2BT.root", isBgonlyStudy, poiVal, nToys, makeTtree ) ;
 
       }
 
