f9d30cc8153ef18d45f392e6052aef73ea440304
kreis <>
2012-09-01 15:28:05 +0000

    add option to save fit result

--- LikelihoodBuilder/minimalFit.C
+++ LikelihoodBuilder/minimalFit.C
@@ -17,10 +17,13 @@ using namespace RooStats ;
 using namespace std;
 
 
-void minimalFit(TString workspaceFile = "test.root", double signalCrossSectionGuess = 50.0, double signalCrossSectionLow = 0.0, double signalCrossSectionHigh = 1000.0) 
+void minimalFit(TString workspaceFile = "test.root", double signalCrossSectionGuess = 50.0, double signalCrossSectionLow = 0.0, double signalCrossSectionHigh = 1000.0, bool updateWS = false) 
 {
-
-  TFile* wstf = new TFile ( workspaceFile );
+  
+  TFile* wstf = 0;  
+  if( updateWS == true ) wstf = new TFile ( workspaceFile, "UPDATE", "", 1);
+  else wstf = new TFile ( workspaceFile, "READ", "", 1);
+    
   
   RooWorkspace* ws = (RooWorkspace*)wstf->Get("workspace");
 
@@ -37,6 +40,9 @@ void minimalFit(TString workspaceFile = "test.root", double signalCrossSectionGu
   fitResult->Print();
   cout << "RooFitResult status = " << fitResult->status() << endl;
   cout << "RooFitResult minNll = " << fitResult->minNll() << endl;
+
+  if(updateWS) ws->Write();
+  wstf->Close();
 
   return;
 }
