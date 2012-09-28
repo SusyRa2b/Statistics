1d1f0d1e0f7e1268b228e659508bf2e3cfb6af5f
kreis <>
2012-09-28 13:33:30 +0000

    perl script to translate toy inputs from owen/ale/keith

---
+++ LikelihoodBuilder/doTranslate.pl
@@ -0,0 +1,25 @@
+#!/user/bin/perl
+use strict;
+use warnings;
+
+opendir my($dh), $ARGV[0] or die  "Couldn't open dir '$ARGV[0]': $!";
+my @list = grep {!/^\./ }readdir $dh;
+closedir $dh;
+
+open(my $fout, ">", "$ARGV[0]/$ARGV[0].dat") or die "Couldn't open output : $!";
+
+foreach(@list) {
+  
+  my $file = $ARGV[0]."/".$_;
+  
+  print "Translating $file...\n";
+  system("perl translate.pl $file");
+  
+  
+  if($file =~ /(\S*)(.dat)/) {
+    system("cp signalModelFile.dat $1/.");
+    print $fout "$1\n";
+  }
+}
+
+close $fout;
