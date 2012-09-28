d548ccdf5712c8b17c2452e310f2b218533e9db6
kreis <>
2012-09-28 14:14:53 +0000

    fix typo

--- LikelihoodBuilder/setupAnalysis.C
+++ LikelihoodBuilder/setupAnalysis.C
@@ -4,6 +4,6 @@ void setupAnalysis(){
   gSystem->CompileMacro("minimalFit.C"                      ,"kO") ;
   gSystem->CompileMacro("minimalProfileLikelihood.C"        ,"kO") ;
   gSystem->CompileMacro("analyzeFit.C"                      ,"kO") ;
-  gSystem->CompileMacro("runFits.C"                         ,"kO") ;
+  gSystem->CompileMacro("runFit.C"                         ,"kO") ;
   return;
 }
