52f627edf8a4eb4aae7da3f0ea036f11866f7dc7
owen <>
2012-10-24 12:20:58 +0000

    Owen : more scan graphs

--- 3Dcode/ws_constrained_profile3D.c
+++ 3Dcode/ws_constrained_profile3D.c
@@ -1,5 +1,6 @@
 
 #include "TRandom2.h"
+#include "TLegend.h"
 #include "TFile.h"
 #include "TLine.h"
 #include "TPad.h"
@@ -383,20 +384,28 @@
        double tcmu_znn_Vals[100] ;
        double tcmu_vv_Vals[100] ;
        double tcmu_susy_Vals[100] ;
+       double mu_qcd_ldp_Vals[100] ;
+       double mu_ttwj_sl_Vals[100] ;
        double trigeff_Vals[100] ;
        double trigeff_sl_Vals[100] ;
        double sf_ttwj_Vals[100] ;
        double sf_qcd_Vals[100] ;
-       double pdf_sf_qcd_Vals[100] ;
        double allsmcomps_Vals[100] ;
+       double pdf_sf_qcd_Vals[100] ;
+       double pdf_sf_ttwj_Vals[100] ;
+       double pdf_N_ldp_Vals[100] ;
+       double pdf_N_sl_Vals[100] ;
        double minNllVal(1.e9) ;
 
 
        double allsmcomps_max(0.) ;
+       double nptf_max(0.) ;
+       double ctrlmu_max(0.) ;
+       double pdfs_max(0.) ;
 
        for ( int poivi=0; poivi < npoiPoints ; poivi++ ) {
 
-          double poiValue = poiMinVal + poivi*(poiMaxVal-poiMinVal)/(1.*npoiPoints) ;
+          double poiValue = poiMinVal + poivi*(poiMaxVal-poiMinVal)/(1.*(npoiPoints-1)) ;
 
           rrv_poiValue -> setVal( poiValue ) ;
           rrv_poiValue -> setConstant( kTRUE ) ;
@@ -429,7 +438,41 @@
 
           char pname[1000] ;
           RooAbsReal* par(0x0) ;
-          double mu_qcd(0.), mu_ttwj(0.), mu_znn(0.), mu_vv(0.), mu_susy(0.), trigeff(0.), trigeff_sl(0.), sf_ttwj(0.), sf_qcd(0.), pdf_sf_qcd(0.) ;
+          double mu_qcd(0.), mu_ttwj(0.), mu_znn(0.), mu_vv(0.), mu_susy(0.), trigeff(0.), trigeff_sl(0.), sf_ttwj(0.), sf_qcd(0.) ;
+          double mu_qcd_ldp(0.), mu_ttwj_sl(0.) ;
+          double pdf_sf_qcd(0.), pdf_sf_ttwj(0.), pdf_N_ldp(0.), pdf_N_sl(0.) ;
+
+          sprintf( pname, "pdf_N_ldp_%s", binstring.Data() ) ;
+          par = (RooAbsReal*) ws -> obj( pname ) ;
+          if ( par == 0x0 ) {
+             printf( " *** %s missing from workspace.\n", pname ) ;
+          } else {
+             pdf_N_ldp = par -> getVal() ;
+          }
+
+          sprintf( pname, "pdf_N_1lep_%s", binstring.Data() ) ;
+          par = (RooAbsReal*) ws -> obj( pname ) ;
+          if ( par == 0x0 ) {
+             printf( " *** %s missing from workspace.\n", pname ) ;
+          } else {
+             pdf_N_sl = par -> getVal() ;
+          }
+
+          sprintf( pname, "pdf_sf_qcd_%s", binstring.Data() ) ;
+          par = (RooAbsReal*) ws -> obj( pname ) ;
+          if ( par == 0x0 ) {
+             printf( " *** %s missing from workspace.\n", pname ) ;
+          } else {
+             pdf_sf_qcd = par -> getVal() ;
+          }
+
+          sprintf( pname, "pdf_sf_ttwj_%s", binstring.Data() ) ;
+          par = (RooAbsReal*) ws -> obj( pname ) ;
+          if ( par == 0x0 ) {
+             printf( " *** %s missing from workspace.\n", pname ) ;
+          } else {
+             pdf_sf_ttwj = par -> getVal() ;
+          }
 
           sprintf( pname, "mu_qcd_%s", binstring.Data() ) ;
           par = (RooAbsReal*) ws -> obj( pname ) ;
@@ -503,17 +546,29 @@
              sf_qcd = par -> getVal() ;
           }
 
-          sprintf( pname, "pdf_sf_qcd_%s", binstring.Data() ) ;
+          sprintf( pname, "mu_qcd_ldp_%s", binstring.Data() ) ;
           par = (RooAbsReal*) ws -> obj( pname ) ;
           if ( par == 0x0 ) {
              printf( " *** %s missing from workspace.\n", pname ) ;
           } else {
-             pdf_sf_qcd = par -> getVal() ;
+             mu_qcd_ldp = par -> getVal() ;
+          }
+
+          sprintf( pname, "mu_ttwj_sl_%s", binstring.Data() ) ;
+          par = (RooAbsReal*) ws -> obj( pname ) ;
+          if ( par == 0x0 ) {
+             printf( " *** %s missing from workspace.\n", pname ) ;
+          } else {
+             mu_ttwj_sl = par -> getVal() ;
           }
 
 
-          printf(" *** debug : mu_qcd=%5.1f, mu_ttwj=%5.1f, mu_znn=%5.1f, mu_vv=%5.1f, mu_susy=%5.1f, trigeff=%5.3f, trigeff_sl=%5.3f, sf_ttwj=%5.3f, sf_qcd=%6.3f, pdf_sf_qcd=%12.10f\n",
-              mu_qcd, mu_ttwj, mu_znn, mu_vv, mu_susy, trigeff, trigeff_sl, sf_ttwj, sf_qcd, pdf_sf_qcd ) ;
+          printf(" *** debug1 : mu_qcd=%6.2f, mu_ttwj=%6.2f, mu_znn=%6.2f, mu_vv=%6.2f, mu_susy=%6.2f, trigeff=%5.3f, trigeff_sl=%5.3f\n",
+              mu_qcd, mu_ttwj, mu_znn, mu_vv, mu_susy, trigeff, trigeff_sl ) ;
+          printf(" *** debug2 : mu_qcd_ldp=%6.2f, mu_ttwj_sl=%6.2f, sf_ttwj=%5.3f, sf_qcd=%6.3f\n",
+              mu_qcd_ldp, mu_ttwj_sl, sf_ttwj, sf_qcd ) ;
+          printf(" *** debug3 : pdf_sf_qcd=%12.10f, pdf_sf_ttwj=%12.10f, pdf_N_ldp=%12.10f, pdf_N_sl=%12.10f\n",
+              pdf_sf_qcd, pdf_sf_ttwj, pdf_N_ldp, pdf_N_sl ) ;
           cout << flush ;
 
           tcmu_qcd_Vals[poivi] = trigeff * mu_qcd ;
@@ -521,15 +576,33 @@
           tcmu_znn_Vals[poivi] = trigeff_sl * mu_znn ;
           tcmu_vv_Vals[poivi] = trigeff_sl * mu_vv ;
           tcmu_susy_Vals[poivi] = trigeff_sl * mu_susy ;
+          mu_qcd_ldp_Vals[poivi] = mu_qcd_ldp ;
+          mu_ttwj_sl_Vals[poivi] = mu_ttwj_sl ;
           trigeff_Vals[poivi] = trigeff ;
           trigeff_sl_Vals[poivi] = trigeff_sl ;
           sf_ttwj_Vals[poivi] = sf_ttwj ;
           sf_qcd_Vals[poivi] = sf_qcd ;
-          pdf_sf_qcd_Vals[poivi] = pdf_sf_qcd ;
+          pdf_sf_qcd_Vals[poivi] = -2*log(pdf_sf_qcd) ;
+          pdf_sf_ttwj_Vals[poivi] = -2*log(pdf_sf_ttwj) ;
+          pdf_N_ldp_Vals[poivi] = -2*log(pdf_N_ldp) ;
+          pdf_N_sl_Vals[poivi] = -2*log(pdf_N_sl) ;
 
           allsmcomps_Vals[poivi] = trigeff * mu_qcd + trigeff_sl * ( mu_ttwj + mu_qcd + mu_znn + mu_vv ) ;
 
           if ( allsmcomps_Vals[poivi] > allsmcomps_max ) { allsmcomps_max = allsmcomps_Vals[poivi] ; }
+
+          if ( sf_ttwj > nptf_max ) { nptf_max = sf_ttwj ; }
+          if ( sf_qcd > nptf_max ) { nptf_max = sf_qcd ; }
+          if ( trigeff > nptf_max ) { nptf_max = trigeff ; }
+          if ( trigeff_sl > nptf_max ) { nptf_max = trigeff_sl ; }
+
+          if ( mu_qcd_ldp > ctrlmu_max ) { ctrlmu_max = mu_qcd_ldp ; }
+          if ( mu_ttwj_sl > ctrlmu_max ) { ctrlmu_max = mu_ttwj_sl ; }
+
+          if ( -2*log(pdf_sf_qcd) > pdfs_max ) { pdfs_max = -2*log(pdf_sf_qcd) ; }
+          if ( -2*log(pdf_sf_ttwj) > pdfs_max ) { pdfs_max = -2*log(pdf_sf_ttwj) ; }
+          if ( -2*log(pdf_N_ldp) > pdfs_max ) { pdfs_max = -2*log(pdf_N_ldp) ; }
+          if ( -2*log(pdf_N_sl) > pdfs_max ) { pdfs_max = -2*log(pdf_N_sl) ; }
 
 
           poiVals[poivi] = new_poi_rar->getVal() ;
@@ -603,7 +676,7 @@
        }
 
        char htitle[1000] ;
-       sprintf(htitle, "%s profile likelihood scan", new_poi_name ) ;
+       sprintf(htitle, "%s profile likelihood scan: -2ln(L/Lm)", new_poi_name ) ;
        TH1F* hscan = new TH1F("hscan", htitle, 10, poiMinVal, poiMaxVal ) ;
        hscan->SetMinimum(0.) ;
        hscan->SetMaximum(ymax) ;
@@ -666,30 +739,44 @@
        gr_tcmu_qcd->SetName( gname ) ;
        gr_tcmu_qcd->SetLineWidth( 2 ) ;
        gr_tcmu_qcd->SetLineColor( 2 ) ;
+       gr_tcmu_qcd->SetFillColor( kWhite ) ;
 
        TGraph* gr_tcmu_ttwj = new TGraph( npoiPoints, poiVals, tcmu_ttwj_Vals ) ;
        sprintf( gname, "scan_%s_tcmu_ttwj", new_poi_name ) ;
        gr_tcmu_ttwj->SetName( gname ) ;
        gr_tcmu_ttwj->SetLineWidth( 2 ) ;
        gr_tcmu_ttwj->SetLineColor( 4 ) ;
+       gr_tcmu_ttwj->SetFillColor( kWhite ) ;
 
        TGraph* gr_tcmu_znn = new TGraph( npoiPoints, poiVals, tcmu_znn_Vals ) ;
        sprintf( gname, "scan_%s_tcmu_znn", new_poi_name ) ;
        gr_tcmu_znn->SetName( gname ) ;
        gr_tcmu_znn->SetLineWidth( 2 ) ;
        gr_tcmu_znn->SetLineColor( 3 ) ;
+       gr_tcmu_znn->SetFillColor( kWhite ) ;
 
        TGraph* gr_tcmu_vv = new TGraph( npoiPoints, poiVals, tcmu_vv_Vals ) ;
        sprintf( gname, "scan_%s_tcmu_vv", new_poi_name ) ;
        gr_tcmu_vv->SetName( gname ) ;
        gr_tcmu_vv->SetLineWidth( 2 ) ;
-       gr_tcmu_vv->SetLineColor( 2 ) ;
+       gr_tcmu_vv->SetLineColor( kOrange+1 ) ;
+       gr_tcmu_vv->SetFillColor( kWhite ) ;
 
        TGraph* gr_allsmcomps = new TGraph( npoiPoints, poiVals, allsmcomps_Vals ) ;
        sprintf( gname, "scan_%s_allsmcomps", new_poi_name ) ;
        gr_allsmcomps->SetName( gname ) ;
        gr_allsmcomps->SetLineWidth( 2 ) ;
        gr_allsmcomps->SetLineColor( 1 ) ;
+       gr_allsmcomps->SetFillColor( kWhite ) ;
+
+
+       TLegend* legend_comps = new TLegend(0.7,0.8, 0.85, 0.95) ;
+       legend_comps->SetFillColor(kWhite) ;
+       legend_comps -> AddEntry( gr_allsmcomps, "All SM" ) ;
+       legend_comps -> AddEntry( gr_tcmu_qcd, "qcd" ) ;
+       legend_comps -> AddEntry( gr_tcmu_ttwj, "ttwj" ) ;
+       legend_comps -> AddEntry( gr_tcmu_znn, "Znn" ) ;
+       legend_comps -> AddEntry( gr_tcmu_vv, "VV" ) ;
 
 
        sprintf(htitle, "%s SM components (in events)", new_poi_name ) ;
@@ -704,6 +791,8 @@
        gr_tcmu_vv->Draw("c") ;
        gr_allsmcomps->Draw("c") ;
 
+       legend_comps->Draw() ;
+
        gPad->SetGridx(1) ;
        gPad->SetGridy(1) ;
 
@@ -714,6 +803,237 @@
 
        printf("\n Saving %s\n", outpdffile ) ;
        ccomps->SaveAs( outpdffile ) ;
+
+
+
+
+
+
+
+
+      //--- canvas with NP scale factors and trig eff.
+
+       TCanvas* cnptf = (TCanvas*) gDirectory->FindObject("cnptf") ;
+       if ( cnptf == 0x0 ) {
+          printf("\n Creating canvas.\n\n") ;
+          cnptf = new TCanvas("cnptf","Components") ;
+       }
+
+       TGraph* gr_trigeff = new TGraph( npoiPoints, poiVals, trigeff_Vals ) ;
+       sprintf( gname, "scan_%s_trigeff", new_poi_name ) ;
+       gr_trigeff->SetName( gname ) ;
+       gr_trigeff->SetLineWidth( 2 ) ;
+       gr_trigeff->SetLineColor( 1 ) ;
+       gr_trigeff->SetLineStyle( 2 ) ;
+       gr_trigeff->SetFillColor( kWhite ) ;
+
+       TGraph* gr_trigeff_sl = new TGraph( npoiPoints, poiVals, trigeff_sl_Vals ) ;
+       sprintf( gname, "scan_%s_trigeff_sl", new_poi_name ) ;
+       gr_trigeff_sl->SetName( gname ) ;
+       gr_trigeff_sl->SetLineWidth( 2 ) ;
+       gr_trigeff_sl->SetLineColor( 1 ) ;
+       gr_trigeff_sl->SetLineStyle( 3 ) ;
+       gr_trigeff_sl->SetFillColor( kWhite ) ;
+
+       TGraph* gr_sf_ttwj = new TGraph( npoiPoints, poiVals, sf_ttwj_Vals ) ;
+       sprintf( gname, "scan_%s_sf_ttwj", new_poi_name ) ;
+       gr_sf_ttwj->SetName( gname ) ;
+       gr_sf_ttwj->SetLineWidth( 2 ) ;
+       gr_sf_ttwj->SetLineColor( 4 ) ;
+       gr_sf_ttwj->SetFillColor( kWhite ) ;
+
+       TGraph* gr_sf_qcd = new TGraph( npoiPoints, poiVals, sf_qcd_Vals ) ;
+       sprintf( gname, "scan_%s_sf_qcd", new_poi_name ) ;
+       gr_sf_qcd->SetName( gname ) ;
+       gr_sf_qcd->SetLineWidth( 2 ) ;
+       gr_sf_qcd->SetLineColor( 2 ) ;
+       gr_sf_qcd->SetFillColor( kWhite ) ;
+
+
+       TLegend* legend_nptf = new TLegend(0.7,0.8, 0.85, 0.95) ;
+       legend_nptf->SetFillColor(kWhite) ;
+       legend_nptf -> AddEntry( gr_trigeff, "Trig eff, 0lep" ) ;
+       legend_nptf -> AddEntry( gr_trigeff_sl, "Trig eff, 1lep" ) ;
+       legend_nptf -> AddEntry( gr_sf_ttwj, "SF ttwj" ) ;
+       legend_nptf -> AddEntry( gr_sf_qcd, "SF QCD" ) ;
+
+
+       sprintf(htitle, "%s Closure NPs and trigger eff.", new_poi_name ) ;
+       TH1F* hnptf = new TH1F("hnptf", htitle, 10, poiMinVal, poiMaxVal ) ;
+       hnptf->SetMinimum(0.) ;
+       hnptf->SetMaximum(1.1 * nptf_max ) ;
+
+       hnptf->Draw() ;
+       gr_trigeff->Draw("c") ;
+       gr_trigeff_sl->Draw("c") ;
+       gr_sf_ttwj->Draw("c") ;
+       gr_sf_qcd->Draw("c") ;
+
+       legend_nptf->Draw() ;
+
+       gPad->SetGridx(1) ;
+       gPad->SetGridy(1) ;
+
+       sprintf( outrootfile, "%s/scan-%s-nptf.root", outputdir.Data(), new_poi_name ) ;
+       sprintf( outpdffile, "%s/scan-%s-nptf.pdf", outputdir.Data(), new_poi_name ) ;
+
+       cnptf->Update() ; cnptf->Draw() ;
+
+       printf("\n Saving %s\n", outpdffile ) ;
+       cnptf->SaveAs( outpdffile ) ;
+
+
+
+
+
+
+
+
+
+      //--- canvas with control sample mus.
+
+       TCanvas* cctrlmu = (TCanvas*) gDirectory->FindObject("cctrlmu") ;
+       if ( cctrlmu == 0x0 ) {
+          printf("\n Creating canvas.\n\n") ;
+          cctrlmu = new TCanvas("cctrlmu","Control sample mus") ;
+       }
+
+       TGraph* gr_mu_qcd_ldp = new TGraph( npoiPoints, poiVals, mu_qcd_ldp_Vals ) ;
+       sprintf( gname, "scan_%s_mu_qcd_ldp", new_poi_name ) ;
+       gr_mu_qcd_ldp->SetName( gname ) ;
+       gr_mu_qcd_ldp->SetLineWidth( 2 ) ;
+       gr_mu_qcd_ldp->SetLineColor( 2 ) ;
+       gr_mu_qcd_ldp->SetLineStyle( 1 ) ;
+       gr_mu_qcd_ldp->SetFillColor( kWhite ) ;
+
+
+       TGraph* gr_mu_ttwj_sl = new TGraph( npoiPoints, poiVals, mu_ttwj_sl_Vals ) ;
+       sprintf( gname, "scan_%s_mu_ttwj_sl", new_poi_name ) ;
+       gr_mu_ttwj_sl->SetName( gname ) ;
+       gr_mu_ttwj_sl->SetLineWidth( 2 ) ;
+       gr_mu_ttwj_sl->SetLineColor( 4 ) ;
+       gr_mu_ttwj_sl->SetLineStyle( 1 ) ;
+       gr_mu_ttwj_sl->SetFillColor( kWhite ) ;
+
+
+       TLegend* legend_ctrlmu = new TLegend(0.7,0.8, 0.85, 0.95) ;
+       legend_ctrlmu->SetFillColor(kWhite) ;
+
+       legend_ctrlmu->AddEntry( gr_mu_qcd_ldp, "mu qcd LDP") ;
+       legend_ctrlmu->AddEntry( gr_mu_ttwj_sl, "mu ttwj SL") ;
+
+
+       sprintf(htitle, "%s Control sample mus", new_poi_name ) ;
+       TH1F* hctrlmu = new TH1F("hctrlmu", htitle, 10, poiMinVal, poiMaxVal ) ;
+       hctrlmu->SetMinimum(0.) ;
+       hctrlmu->SetMaximum(1.1 * ctrlmu_max ) ;
+
+       hctrlmu->Draw() ;
+       gr_mu_qcd_ldp->Draw("c") ;
+       gr_mu_ttwj_sl->Draw("c") ;
+
+       legend_ctrlmu->Draw() ;
+
+       gPad->SetGridx(1) ;
+       gPad->SetGridy(1) ;
+
+       sprintf( outrootfile, "%s/scan-%s-ctrlmu.root", outputdir.Data(), new_poi_name ) ;
+       sprintf( outpdffile, "%s/scan-%s-ctrlmu.pdf", outputdir.Data(), new_poi_name ) ;
+
+       cctrlmu->Update() ; cctrlmu->Draw() ;
+
+       printf("\n Saving %s\n", outpdffile ) ;
+       cctrlmu->SaveAs( outpdffile ) ;
+
+
+
+
+
+
+
+
+
+      //--- canvas with pdfs.
+
+       TCanvas* cpdfs = (TCanvas*) gDirectory->FindObject("cpdfs") ;
+       if ( cpdfs == 0x0 ) {
+          printf("\n Creating canvas.\n\n") ;
+          cpdfs = new TCanvas("cpdfs","-ln(PDF)") ;
+       }
+
+       TGraph* gr_pdf_sf_qcd = new TGraph( npoiPoints, poiVals, pdf_sf_qcd_Vals ) ;
+       sprintf( gname, "scan_%s_pdf_sf_qcd", new_poi_name ) ;
+       gr_pdf_sf_qcd->SetName( gname ) ;
+       gr_pdf_sf_qcd->SetLineWidth( 2 ) ;
+       gr_pdf_sf_qcd->SetLineColor( 2 ) ;
+       gr_pdf_sf_qcd->SetLineStyle( 1 ) ;
+       gr_pdf_sf_qcd->SetFillColor( kWhite ) ;
+
+
+       TGraph* gr_pdf_sf_ttwj = new TGraph( npoiPoints, poiVals, pdf_sf_ttwj_Vals ) ;
+       sprintf( gname, "scan_%s_pdf_sf_ttwj", new_poi_name ) ;
+       gr_pdf_sf_ttwj->SetName( gname ) ;
+       gr_pdf_sf_ttwj->SetLineWidth( 2 ) ;
+       gr_pdf_sf_ttwj->SetLineColor( 4 ) ;
+       gr_pdf_sf_ttwj->SetLineStyle( 1 ) ;
+       gr_pdf_sf_ttwj->SetFillColor( kWhite ) ;
+
+
+       TGraph* gr_pdf_N_ldp = new TGraph( npoiPoints, poiVals, pdf_N_ldp_Vals ) ;
+       sprintf( gname, "scan_%s_pdf_N_ldp", new_poi_name ) ;
+       gr_pdf_N_ldp->SetName( gname ) ;
+       gr_pdf_N_ldp->SetLineWidth( 2 ) ;
+       gr_pdf_N_ldp->SetLineColor( 2 ) ;
+       gr_pdf_N_ldp->SetLineStyle( 2 ) ;
+       gr_pdf_N_ldp->SetFillColor( kWhite ) ;
+
+
+       TGraph* gr_pdf_N_sl = new TGraph( npoiPoints, poiVals, pdf_N_sl_Vals ) ;
+       sprintf( gname, "scan_%s_pdf_N_sl", new_poi_name ) ;
+       gr_pdf_N_sl->SetName( gname ) ;
+       gr_pdf_N_sl->SetLineWidth( 2 ) ;
+       gr_pdf_N_sl->SetLineColor( 4 ) ;
+       gr_pdf_N_sl->SetLineStyle( 2 ) ;
+       gr_pdf_N_sl->SetFillColor( kWhite ) ;
+
+
+       TLegend* legend_pdfs = new TLegend(0.7,0.8, 0.85, 0.95) ;
+       legend_pdfs->SetFillColor(kWhite) ;
+
+       legend_pdfs->AddEntry( gr_pdf_sf_qcd, "-2ln(pdf_sf_qcd)") ;
+       legend_pdfs->AddEntry( gr_pdf_sf_ttwj, "-2ln(pdf_sf_ttwj)") ;
+       legend_pdfs->AddEntry( gr_pdf_N_ldp, "-2ln(pdf_N_ldp)") ;
+       legend_pdfs->AddEntry( gr_pdf_N_sl, "-2ln(pdf_N_sl)") ;
+
+
+       sprintf(htitle, "%s -2ln(PDF) vals", new_poi_name ) ;
+       TH1F* hpdfs = new TH1F("hpdfs", htitle, 10, poiMinVal, poiMaxVal ) ;
+       hpdfs->SetMinimum(0.) ;
+       hpdfs->SetMaximum(1.1 * pdfs_max ) ;
+
+       hpdfs->Draw() ;
+       gr_pdf_sf_qcd->Draw("c") ;
+       gr_pdf_sf_ttwj->Draw("c") ;
+       gr_pdf_N_ldp->Draw("c") ;
+       gr_pdf_N_sl->Draw("c") ;
+
+       legend_pdfs->Draw() ;
+
+       gPad->SetGridx(1) ;
+       gPad->SetGridy(1) ;
+
+       sprintf( outrootfile, "%s/scan-%s-pdfs.root", outputdir.Data(), new_poi_name ) ;
+       sprintf( outpdffile, "%s/scan-%s-pdfs.pdf", outputdir.Data(), new_poi_name ) ;
+
+       cpdfs->Update() ; cpdfs->Draw() ;
+
+       printf("\n Saving %s\n", outpdffile ) ;
+       cpdfs->SaveAs( outpdffile ) ;
+
+
+
+
+
 
 
 
