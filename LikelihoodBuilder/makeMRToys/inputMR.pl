5e631c0713986610fa9db73d565e7717a5474c14
kreis <>
2013-01-08 12:26:35 +0000

    to save Poisson means for MR counts

---
+++ LikelihoodBuilder/makeMRToys/inputMR.pl
@@ -0,0 +1,60 @@
+#/user/bin/perl
+use strict;
+use warnings;
+
+open (my $fin, "<", "listMR.txt") or die "Can't open input file.";
+open (my $fout, ">", "saveInputMR.dat") or die "Can't open input file.";
+
+
+while(<$fin>)
+  {
+
+    if(/(\S+)/)
+      {
+	my $fileName = $1;
+
+	if(/(\d)(\d)(\d)/)
+	  {
+
+	    my $ht = $1;
+	    my $met = $2;
+	    my $btag = $3;
+	    
+	    $ht+=1;
+	    $met+=1;
+
+	    open (my $fc, "<", "$fileName");
+	    
+	    my $line = 0;
+	    while(<$fc>)
+	      {
+		if(/(\S+)/)
+		  {
+		    my $value = $1;
+
+		    my $binName = "M$met"."_H$ht"."_$btag"."b";
+
+		    $line++;
+		    if($line==1){ print $fout "N_MRzeroLepton_TopWJets_$binName $value\n"; }
+		    if($line==2){ print $fout "N_oneTightMu_Theta1_TopWJets_$binName $value\n"; }
+		    if($line==3){ print $fout "N_oneLooseLep_Theta1_TopWJets_$binName $value\n"; }
+		    if($line==4){ print $fout "N_oneTightMu_Theta2_TopWJets_$binName $value\n"; }
+		    if($line==5){ print $fout "N_oneLooseLep_Theta2_TopWJets_$binName $value\n"; }
+		    if($line==6){ print $fout "N_oneTightMu_Theta3_TopWJets_$binName $value\n"; }
+		    if($line==7){ print $fout "N_oneLooseLep_Theta3_TopWJets_$binName $value\n"; }
+		    if($line==8){ print $fout "N_oneTightMu_Theta4_TopWJets_$binName $value\n"; }
+		    if($line==9){ print $fout "N_oneLooseLep_Theta4_TopWJets_$binName $value\n"; }
+		    if($line==10){ print $fout "N_oneTightMu_Theta5_TopWJets_$binName $value\n"; }
+		    if($line==11){ print $fout "N_oneLooseLep_Theta5_TopWJets_$binName $value\n"; }
+		    if($line==12){ print $fout "N_twoTightMu_TopWJets_$binName $value\n"; }
+		    if($line==13){ print $fout "N_twoLooseLep_TopWJets_$binName $value\n"; }
+		  }
+	      }	
+	  }
+      }
+  }
+
+
+
+
+
