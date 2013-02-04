d428525d4c7dd49e8a0c61b58e57f0429fad48bc
kflowers <>
2013-02-04 22:38:38 +0000

    metReweightingBuilderSIMPLETAU.C

--- LikelihoodBuilder/metReweightingBuilderSIMPLETAU.h
+++ LikelihoodBuilder/metReweightingBuilderSIMPLETAU.h
@@ -10,11 +10,7 @@ void makeDileptonConstraintsPredictions( RooWorkspace& wspace, TString binname, 
 
 void makeTauHadBinPrediction( RooWorkspace& wspace, TString binname, TString binname_outside );
 
-<<<<<<< metReweightingBuilderSIMPLETAU.h
-void buildMRLikelihood( RooWorkspace& wspace, TString outputFile, TString setupFileName, bool standalone, TRandom2& rd, TString outputTextFile ); 
-=======
 void buildMRLikelihood( RooWorkspace& wspace, TString outputFile, TString setupFileName, bool standalone, TString nuisanceOption = "allWidths", TString countsPath="" ); 
->>>>>>> 1.4
 
 
 #endif
