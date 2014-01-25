2a22d0004db4e80a97605de33636d0de7ae9a4ea
gaz <alessandro.gaz@cern.ch>
2014-01-25 19:10:39 +0100

    added some more realistic systematics

---
+++ 3Dcode_ewkino/GenerateDummyBtag.C
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TFile.h"
+#include "TChain.h"
+#include "TString.h"
+#include "TROOT.h"
+
+void GenerateDummyBtag() {
+
+  gROOT->Reset();
+
+  // get binning from "Binning.txt"
+
+  int in_version, in_nBinsVar1, in_nBinsVar2, in_nBinsBjets ;
+  TString label, in_Var1, in_Var2, in_Var3 ;
+
+  ifstream inBinning ;
+  inBinning.open("Binning.txt") ;
+
+  inBinning >> label >> in_Var1 ;
+  inBinning >> label >> in_Var2 ;
+  inBinning >> label >> in_Var3 ;
+
+  inBinning >> label >> in_nBinsVar1 ;
+  inBinning >> label >> in_nBinsVar2 ;
+  inBinning >> label >> in_nBinsBjets ;
+
+  const int nBinsVar1  = in_nBinsVar1 ;
+  const int nBinsVar2  = in_nBinsVar2 ;
+  const int nBinsBjets = in_nBinsBjets ;
+
+  inBinning.close() ;
+
+  TString aVar1 = in_Var1 ;
+  TString aVar2 = in_Var2 ;
+  if ( aVar1 == "MET/sqrt(HT)" ) aVar1 = "MET_div_sqrtHT" ;
+  if ( aVar2 == "MET/sqrt(HT)" ) aVar2 = "MET_div_sqrtHT" ;
+
+  int nBins = nBinsVar1 * nBinsVar2 * nBinsBjets ;
+
+  double dummySig[3] = {-0.05,0.02,0.07} ;
+  double dummySLSig = 0.01 ;
+  double dummySL    = 0.01 ;
+  double dummyLdp   = 0.01 ;
+
+  char outfile[10000] ;
+  sprintf( outfile, "datfiles/DummyBtagEff-%s-%d-%s-%d-nB%d.dat", aVar1.Data(), nBinsVar1, aVar2.Data(), nBinsVar2, nBinsBjets ) ;
+
+  ofstream outDummy;
+  outDummy.open(outfile);
+
+
+  // generate dummy systematics for a bunch of points 
+
+  int mGls[9] = {150,200,250,300,350,400,450,500,550};
+  int mLsps[9] = {0,0,0,0,0,0,0,0,0};
+
+
+  outDummy << -1 << " " << -1 << " " ;
+
+  for ( int i = 0 ; i < nBins ; i++ ) {
+    outDummy << dummySig[i%3] << " " ;
+  }
+    
+  for ( int i = 0 ; i < nBins ; i++ ) {
+    outDummy << dummySLSig << " " ;
+  }
+  
+  for ( int i = 0 ; i < nBins ; i++ ) {
+    outDummy << dummySL << " " ;
+  }
+    
+  for ( int i = 0 ; i < nBins ; i++ ) {
+    outDummy << dummyLdp << " " ;
+  }
+  
+  outDummy << endl ;
+
+
+  for ( int iGl = 0 ; iGl < 9 ; iGl++ ) {
+
+    int mGl  = mGls[iGl];
+    int mLsp = mLsps[iGl];
+
+    outDummy << mGl << " " << mLsp << " " ;
+
+    for ( int i = 0 ; i < nBins ; i++ ) {
+      outDummy << dummySig << " " ;
+    }
+    
+    for ( int i = 0 ; i < nBins ; i++ ) {
+      outDummy << dummySLSig << " " ;
+    }
+    
+    for ( int i = 0 ; i < nBins ; i++ ) {
+      outDummy << dummySL << " " ;
+    }
+    
+    for ( int i = 0 ; i < nBins ; i++ ) {
+      outDummy << dummyLdp << " " ;
+    }
+
+    outDummy << endl ;
+    
+  }
+
+  return;
+
+}
